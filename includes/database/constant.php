3de9a67953bb0e0286f38ee24dea1681e4b9131a
Gregg Code <118491204+jGreggCode@users.noreply.github.com>
2025-06-17 15:05:25 +0800

    Create constant.php

---
+++ includes/database/constant.php
@@ -0,0 +1,20 @@
+<?php
+
+// Root url for the site
+date_default_timezone_set('Asia/Manila');
+define('ROOT_URL', 'http://localhost/attendance/public/');
+// Database parameters
+// Data source name
+define('DSN', 'mysql:host=localhost;dbname=saclitms');
+
+// Hostname
+define('DB_HOST', 'localhost');
+
+// DB user
+define('DB_USER', 'root');
+
+// DB password
+define('DB_PASSWORD', '');
+
+// DB name
+define('DB_NAME', 'saclitms');
